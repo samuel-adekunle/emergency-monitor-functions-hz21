4f36b7f5bf54a4fafbe4516cc5e53c2974f60f1b
Samuel Adekunle <me@samuel-adekunle.software>
2021-09-25 18:59:55 +0100

    minimal function

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -27,5 +27,7 @@ module.exports = {
   rules: {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
+    "max-len": 0,
+    "object-curly-spacing": 0,
   },
 };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,37 @@
+import { firestore, initializeApp, credential } from "firebase-admin";
 import * as functions from "firebase-functions";
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", { structuredData: true });
-    response.send("Hello from Firebase!");
+
+export const sendEmergencyEmailRequest = functions.region("europe-west1").database.ref("users/{userId}/emergencies/{emergencyId}").onCreate(async (dataSnapshot, ctx) => {
+  const app = initializeApp({
+    credential: credential.cert("../emergency-monitor-hz21-firebase-adminsdk-4q9bw-e7f1d7320f.json"),
+    databaseURL: "https://emergency-monitor-hz21-default-rtdb.europe-west1.firebasedatabase.app",
+  });
+
+  const emergencyContacts = await dataSnapshot.ref.parent?.parent?.child("emergencyContacts").get();
+
+  if (!emergencyContacts?.exists) {
+    return functions.logger.error("No emergency contacts exists for this user");
+  }
+
+  const emergencyContactsVal = emergencyContacts.val();
+  const emergencyDataVal = dataSnapshot.val();
+
+  functions.logger.log("Emergency: ", emergencyDataVal);
+  functions.logger.log("Emergency Contacts: ", emergencyContactsVal);
+
+  firestore(app).collection("emergencies").add({
+    to: ["ebnsamuel@gmail.com"],
+    message: {
+      subject: "A new Emergency was detected",
+      text: JSON.stringify(emergencyDataVal),
+    },
+  });
+});
+
+export const helloWorld = functions.region("europe-west1").https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {
+    structuredData: true,
+  });
+  response.send("Hello from Firebase!");
 });
